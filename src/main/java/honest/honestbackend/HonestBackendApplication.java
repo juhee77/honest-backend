ceeb84ba4f724b0b4064d3c1a93750dd49c355be
juhee77 <bora2233@naver.com>
2022-03-25 01:21:53 +0900

    Initial commit

---
+++ src/main/java/honest/honestbackend/HonestBackendApplication.java
@@ -0,0 +1,13 @@
+package honest.honestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonestBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/honest/honestbackend/HonestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package honest.honestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
