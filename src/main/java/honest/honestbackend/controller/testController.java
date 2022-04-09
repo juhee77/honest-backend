6cb17fe0cac79a2bbebc6f11cf5920da7d3d5df0
lahezy <jouhi3322@gmail.com>
2022-04-10 01:00:23 +0900

    http test

---
+++ src/main/java/honest/honestbackend/controller/testController.java
@@ -0,0 +1,33 @@
+package honest.honestbackend.controller;
+
+import com.nimbusds.jose.shaded.json.JSONArray;
+import com.nimbusds.jose.shaded.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class testController {
+
+    @ResponseBody
+
+    @RequestMapping(value="/json.do" , produces="application/json; charset=utf-8")  // json.do 라는 객체로
+    public JSONObject json() {
+        JSONObject jsonMain=new JSONObject();										// json 객체 [{변수명:값, 변수명:값}]
+        JSONArray jArray=new JSONArray();											// json 배열
+        JSONObject row = new JSONObject();
+        row.put("name", "naver");
+        row.put("email", "naver@naver.com");
+        jArray.add(0, row);
+
+        JSONObject row2 = new JSONObject();
+        row2.put("name", "kokanry");
+        row2.put("email", "kokanry@naver.com");
+        jArray.add(1, row2);              // jArray = members에 들어갈 변수명,값들
+
+        jsonMain.put("members", jArray);  // jsonMain = members, books, items 같은거 여러개 가능
+        return jsonMain;
+    }
+}
