adbe9fd7bae708fc3b0f6df41adbb7bfd600e382
dlwltn0350 <dlwltn0350@naver.com>
2022-04-14 16:30:56 +0900

    add profile

--- src/main/java/honest/honestbackend/HonestBackendApplication.java
+++ src/main/java/honest/honestbackend/HonestBackendApplication.java
@@ -2,8 +2,9 @@ package honest.honestbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HonestBackendApplication {
 
 	public static void main(String[] args) {
