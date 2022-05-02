561f246988dd06bc163754b9f4a099e0d3c4fa5c
dlwltn0350 <dlwltn0350@naver.com>
2022-05-02 18:22:06 +0900

    meal, dailymeal controller부분 수정 - meal, dailymeal 데이터 업데이트

--- src/main/java/honest/honestbackend/controller/mealController.java
+++ src/main/java/honest/honestbackend/controller/mealController.java
@@ -1,21 +1,15 @@
 package honest.honestbackend.controller;
 
-import honest.honestbackend.domain.Dailymeal;
+import honest.honestbackend.domain.FoodData;
 import honest.honestbackend.domain.Meal;
 import honest.honestbackend.service.dailymealRepository;
-import honest.honestbackend.service.dailymealService;
-import honest.honestbackend.service.mealRepository;
-import honest.honestbackend.service.mealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class mealController {
@@ -31,22 +25,23 @@ public class mealController {
     @ResponseBody
     @PostMapping("/mealSave.do")
     public String mealSavePost(Meal meal){
-
         System.out.println(meal.getMealname());
         String userid;
         Date savetime;
         long dailymealid;
+        int timeflag;
 
         userid = meal.getUserid();
         savetime = meal.getSavetime();
         dailymealid=meal.getDailymealid();
+        timeflag=meal.getTimeflag();//식단position
 
         //meal.setDailymealid(dailymeal.getDailymealid()); // meal dailymeal의 키를 meal 데이터에서 가져와서 설정
-        meal.setMealid(mealRepository.countAllBy()+meal.getMealid()); //meal id설정
+        //meal.setMealid(mealRepository.countAllBy()+meal.getMealid()); //meal id설정
         //mealService.saveMeal(meal);
         mealRepository.save(meal);
-        List<Meal> mealList = mealRepository.findBymealId(userid,dailymealid);
-
+        //List<Meal> mealList = mealRepository.findBymealId(userid,dailymealid);
+        List<Meal> mealList=mealRepository.selectByItem(userid, savetime, timeflag);
         dailymealService.updateDailyMeal(userid, savetime , mealList);
         /*
         Dailymeal dailymeal = dailymealService.checkDailyMeal(userid,savetime);
@@ -60,6 +55,40 @@ public class mealController {
         //System.out.println("dailymeal 저장 !!! "); //없으면 새로 만들고 있으면 update 함
 
         return "";
+    }
+
+    @ResponseBody
+    @GetMapping("/deleteMeal.do")
+    public void deleteMealGet(String userid, Date savetime, int timeflag){//userid, (dailymealid) , savetime, timeflag
+        int calorie=0, protein=0, fat=0, carbohydrate=0;
+
+        if(mealRepository.selectByItem(userid, savetime, timeflag)!=null){
+            List<Meal> mealList=mealRepository.selectByItem(userid, savetime, timeflag);
+
+            /*해당 날짜, position에 총 탄단지가 얼마인지*/
+            for(Meal meal:mealList){
+                calorie+=(int)meal.getCalorie();
+                protein+=(int)meal.getProtein();
+                fat+=(int)meal.getFat();
+                carbohydrate+=(int)meal.getCarbohydrate();
+            }
+
+            mealRepository.deleteByItem(userid,savetime,timeflag); //삭제하고 다시 저장하기 위함
+        }
+        dailymealService.deleteDailyMeal(userid, savetime, calorie, protein, fat, carbohydrate);
+    }
+
+    @ResponseBody
+    @GetMapping("/selectMeal.do")
+    public List<Meal> selectMeal(String userid, Date savetime){
+        if(mealRepository.selectBysaveTime(userid, savetime)!=null){
+            List<Meal> mealList=mealRepository.selectBysaveTime(userid, savetime);
+            return mealList;
+        }
+        else {
+            System.out.println("데이터 없음");
+            return null;
+        }
     }
 
 /*
