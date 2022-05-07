acbb166570e17327c634df3930a5f00c2556ae71
dlwltn0350 <dlwltn0350@naver.com>
2022-05-08 00:25:24 +0900

    dailymeal update방법 수정 (1)

--- src/main/java/honest/honestbackend/controller/mealController.java
+++ src/main/java/honest/honestbackend/controller/mealController.java
@@ -41,8 +41,10 @@ public class mealController {
         //mealService.saveMeal(meal);
         mealRepository.save(meal);
         //List<Meal> mealList = mealRepository.findBymealId(userid,dailymealid);
-        List<Meal> mealList=mealRepository.selectByItem(userid, savetime, timeflag);
-        dailymealService.updateDailyMeal(userid, savetime , mealList);
+
+        //List<Meal> mealList=mealRepository.selectByItem(userid, savetime, timeflag);
+        //dailymealService.updateDailyMeal(userid, savetime , mealList);
+
         //System.out.println("#########"+meal.getIntake());
         /*
         Dailymeal dailymeal = dailymealService.checkDailyMeal(userid,savetime);
@@ -63,52 +65,63 @@ public class mealController {
     public void deleteMealGet(String userid, Date savetime, int timeflag){//userid, (dailymealid) , savetime, timeflag
         int calorie=0, protein=0, fat=0, carbohydrate=0;
 
-        if(mealRepository.selectByItem(userid, savetime, timeflag)!=null){
+        if(!mealRepository.selectByItem(userid, savetime, timeflag).isEmpty()){
+            /*
             List<Meal> mealList=mealRepository.selectByItem(userid, savetime, timeflag);
-
-            /*해당 날짜, position에 총 탄단지가 얼마인지*/
+            해당 날짜, position에 총 탄단지가 얼마인지
             for(Meal meal:mealList){
                 calorie+=(int)(meal.getCalorie()*meal.getIntake());
                 protein+=(int)(meal.getProtein()*meal.getIntake());
                 fat+=(int)(meal.getFat()*meal.getIntake());
                 carbohydrate+=(int)(meal.getCarbohydrate()*meal.getIntake());
-            }
+            }*/
 
             mealRepository.deleteByItem(userid,savetime,timeflag); //삭제하고 다시 저장하기 위함
+
         }
-        dailymealService.deleteDailyMeal(userid, savetime, calorie, protein, fat, carbohydrate);
+        //dailymealService.deleteDailyMeal(userid, savetime, calorie, protein, fat, carbohydrate);
+
     }
 
     @ResponseBody
     @GetMapping("/InitPositionMeal.do")
-    public void InitPositionMealGet(String userid, Date savetime, int timeflag){//userid, (dailymealid) , savetime, timeflag
-        deleteMealGet(userid, savetime, timeflag);
-        int flag=timeflag+1;
-        for(;;){
-            List<Meal> mealList=mealRepository.selectByItem(userid, savetime, flag);
-            if(!mealList.isEmpty()) {
-                for(Meal meal:mealList){
-                    System.out.println(meal.getMealname());
-                    meal.setTimeflag(meal.getTimeflag()-1);//위치 하나씩 줄이기
-                    mealRepository.save(meal);
-                }
-                flag++;
-            }
-            else break;
+    public void InitPositionMealGet(String userid, Date savetime, int timeflag) {//userid, (dailymealid) , savetime, timeflag
+        if (!mealRepository.selectByItem(userid, savetime, timeflag).isEmpty()) {
+            mealRepository.deleteByItem(userid, savetime, timeflag);
         }
-    }
+        List<Meal> ml = mealRepository.selectBysaveTime(userid, savetime);
+        dailymealService.updateDailyMeal(userid, savetime, ml);
 
+        int flag = timeflag + 1;
+            for (; ; ) {
+                List<Meal> mealList = mealRepository.selectByItem(userid, savetime, flag);
+                if (!mealList.isEmpty()) {
+                    for (Meal meal : mealList) {
+                        System.out.println(meal.getMealname());
+                        meal.setTimeflag(meal.getTimeflag() - 1);//위치 하나씩 줄이기
+                        mealRepository.save(meal);
+                    }
+                    flag++;
+                } else break;
+            }
+    }
     @ResponseBody
     @GetMapping("/selectMeal.do")
     public List<Meal> selectMeal(String userid, Date savetime, int timeflag){
-        if(mealRepository.selectByItem(userid, savetime, timeflag)!=null){
-            List<Meal> mealList=mealRepository.selectByItem(userid, savetime, timeflag);
-            return mealList;
-        }
-        else {
-            System.out.println("데이터 없음");
-            return null;
-        }
+        List<Meal> mealList=mealRepository.selectByItem(userid, savetime, timeflag);
+        return mealList;
+    }
+
+    @ResponseBody
+    @GetMapping("/maxTimeFlag.do")
+    public Integer maxTimeFlag(String userid, Date savetime){
+        int timeflag=0;
+        if(!mealRepository.selectBysaveTime(userid, savetime).isEmpty())
+            timeflag=mealRepository.maxTimeFlag(userid, savetime);
+        else
+            timeflag=0;
+        //System.out.println(timeflag);
+        return timeflag;
     }
 
 /*
